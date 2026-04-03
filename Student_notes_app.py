a9806083f8a95e91c0bdaae90f8f49cefd94ec02
Jerome-B-Nuwagaba <Nuwagababill5@gmail.com>
2026-04-03 23:23:48 +0300

    ready to deploy

--- Student_notes_app.py
+++ Student_notes_app.py
@@ -102,10 +102,12 @@ def view():
     return render_template('view.html', courses=courses, notes=notes, coursework=coursework)
 
 # --------- INIT DB ---------
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
 
 
 # --------- templates/base.html ---------
